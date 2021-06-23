9d166fd0ea9377a1574fac1a3ed522d63d26167f
aferreira <aferreira@matterport.com>
2021-06-23 15:34:49 -0300

    updating pokemon list every 24 hours

--- app/src/main/java/com/artur/pokemonapp/di/ViewModelModule.kt
+++ app/src/main/java/com/artur/pokemonapp/di/ViewModelModule.kt
@@ -1,9 +1,14 @@
 package com.artur.pokemonapp.di
 
+import androidx.work.WorkManager
 import com.artur.pokemonapp.ui.pokemonlist.PokemonListViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { PokemonListViewModel(pokemonRepository = get()) }
+
+    factory { WorkManager.getInstance(androidContext()) }
+
+    viewModel { PokemonListViewModel(pokemonRepository = get(), workManager = get()) }
 }
--- app/src/main/java/com/artur/pokemonapp/ui/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/artur/pokemonapp/ui/pokemonlist/PokemonListViewModel.kt
@@ -1,10 +1,27 @@
 package com.artur.pokemonapp.ui.pokemonlist
 
 import androidx.lifecycle.ViewModel
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.artur.pokemonapp.repository.PokemonRepository
+import com.artur.pokemonapp.worker.UpdatePokemonWorker
+import java.util.concurrent.TimeUnit
 
-class PokemonListViewModel(pokemonRepository: PokemonRepository) : ViewModel() {
+class PokemonListViewModel(pokemonRepository: PokemonRepository, val workManager: WorkManager) :
+    ViewModel() {
 
     val pokemonLiveData = pokemonRepository.loadPokemons()
+
+    init {
+        fetchData()
+    }
+
+    fun fetchData() {
+        val updateWorker = PeriodicWorkRequestBuilder<UpdatePokemonWorker>(1, TimeUnit.DAYS)
+            .build()
+
+        workManager.enqueue(updateWorker)
+
+    }
 
 }
